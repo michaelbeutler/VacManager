c5764434baffa20ba649404fe49c279cee464a7d
MichaelBeutler <michael.beutler@gibmit.ch>
2019-04-08 14:03:32 +0200

    test mail

--- includes/accept_vacation.php
+++ includes/accept_vacation.php
@@ -23,6 +23,24 @@ if (isset($_GET['request_id'])) {
     if ($conn->query($sql)) {
         $response->code = 200;
         $response->description = 'success';
+
+        use PHPMailer\PHPMailer\PHPMailer;
+        require '../vendor/autoload.php';
+        //Create a new PHPMailer instance
+        $mail = new PHPMailer;
+        //Set who the message is to be sent from
+        $mail->setFrom('vac-manager@iperka.com', 'Vacation Manager');
+        //Set an alternative reply-to address
+        $mail->addReplyTo('noreply@iperka.com', 'iperka.com');
+        //Set who the message is to be sent to
+        $mail->addAddress('michael.beutler@gibmit.ch', 'Michael Beutler');
+        //Set the subject line
+        $mail->Subject = 'Vacation request accepted';
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        $mail->msgHTML(file_get_contents('../email-templates/accepted_vacation.html'), __DIR__);
+        //Replace the plain text body with one created manually
+        $mail->AltBody = 'This is a plain-text message body';
     } else {
         // error while executing query
         $response->code = 953;

af3c96cdc5e104ef02be00f3b4c395dfb4ed47ed
Michael Beutler <35310806+MichiBeutler@users.noreply.github.com>
2019-05-02 11:32:12 +0200

    Add files via upload

--- assets/jquery.validate/form-validation-init.js
+++ assets/jquery.validate/form-validation-init.js
@@ -35,46 +35,6 @@
             }
         });
 
-        // validate signup form on keyup and submit
-        this.$signupForm.validate({
-            rules: {
-                firstname: "required",
-                lastname: "required",
-                password: {
-                    required: true,
-                    minlength: 5
-                },
-                confirm_password: {
-                    required: true,
-                    minlength: 5,
-                    equalTo: "#password"
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                employer: {
-                    required: true
-                },
-                agree: "required"
-            },
-            messages: {
-                firstname: "Please enter your firstname",
-                lastname: "Please enter your lastname",
-                password: {
-                    required: "Please provide a password",
-                    minlength: "Your password must be at least 5 characters long"
-                },
-                confirm_password: {
-                    required: "Please provide a password",
-                    minlength: "Your password must be at least 5 characters long",
-                    equalTo: "Please enter the same password as above"
-                },
-                email: "Please enter a valid email address",
-                agree: "Please accept our policy"
-            }
-        });
-
         // propose username by combining first- and lastname
         $("#username").focus(function() {
             var firstname = $("#firstname").val();
